f9b7292a102572c062162f3b06f986959413ea40
friendlyhj <youyi580@qq.com>
2020-08-08 11:23:40 +0800

    添加 base 和 jei 依赖

--- src/main/java/youyihj/collision/Collision.java
+++ src/main/java/youyihj/collision/Collision.java
@@ -12,12 +12,13 @@ import youyihj.collision.item.ItemRegistrar;
 import youyihj.collision.recipe.ColliderRecipe;
 import youyihj.collision.recipe.ColliderRecipeRegistrar;
 
-@Mod(modid = Collision.MODID, name = Collision.NAME, version = Collision.VERSION)
+@Mod(modid = Collision.MODID, name = Collision.NAME, version = Collision.VERSION, dependencies = Collision.DEPENDENCIES)
 public class Collision
 {
     public static final String MODID = "collision";
     public static final String NAME = "Collision";
     public static final String VERSION = "1.0.0";
+    public static final String DEPENDENCIES = "required-after:base@[3.13.0,);required-after:jei@[4.15.0.296,)";
 
     private static Logger logger;
 
