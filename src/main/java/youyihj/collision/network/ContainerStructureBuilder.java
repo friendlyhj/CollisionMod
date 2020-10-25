c82b3243dc7e7fed3ce0ea527d639068dc26d8a8
youyihj <youyi580@qq.com>
2020-10-25 22:54:11 +0800

    some fix work

--- src/main/java/youyihj/collision/network/ContainerStructureBuilder.java
+++ src/main/java/youyihj/collision/network/ContainerStructureBuilder.java
@@ -43,6 +43,7 @@ public class ContainerStructureBuilder extends Container {
     @Override
     public ItemStack transferStackInSlot(EntityPlayer playerIn, int index) {
         ItemStack itemstack = ItemStack.EMPTY;
+        if (index < 0) return itemstack;
         Slot slot = this.inventorySlots.get(index);
 
         if (slot != null && slot.getHasStack())
