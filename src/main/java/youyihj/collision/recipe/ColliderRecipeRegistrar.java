caa73b045cfbe2c2eda41742e83c5c19d1b87874
youyihj <youyi580@qq.com>
2020-11-22 16:32:22 +0800

    almost all collider recipe

--- src/main/java/youyihj/collision/recipe/ColliderRecipeRegistrar.java
+++ src/main/java/youyihj/collision/recipe/ColliderRecipeRegistrar.java
@@ -6,30 +6,189 @@ import net.minecraft.item.ItemStack;
 import youyihj.collision.block.absorber.EnumAbsorber;
 import youyihj.collision.item.ItemMaterial;
 
+import static youyihj.collision.block.absorber.EnumAbsorber.NEUTRON;
+import static youyihj.collision.block.absorber.EnumAbsorber.PROTON;
+
 public class ColliderRecipeRegistrar {
     static {
-        new ColliderRecipe(1, new ItemStack(Blocks.STONE), new EnumAbsorber[][] {
-            {EnumAbsorber.NEUTRON, EnumAbsorber.NEUTRON, EnumAbsorber.NEUTRON},
-            {EnumAbsorber.NEUTRON, null, EnumAbsorber.NEUTRON},
-            {EnumAbsorber.NEUTRON, EnumAbsorber.NEUTRON, EnumAbsorber.NEUTRON}
+        new ColliderRecipe(1, new ItemStack(Blocks.COBBLESTONE, 4), new EnumAbsorber[][] {
+                {null, PROTON, null},
+                {null, null, NEUTRON},
+                {null, null, null}
         }).register();
 
-        new ColliderRecipe(1, new ItemStack(Items.BREAD), new EnumAbsorber[][]{
-                {EnumAbsorber.PROTON, null, EnumAbsorber.PROTON},
+        new ColliderRecipe(1, new ItemStack(Items.DYE, 2, 15), new EnumAbsorber[][]{
                 {null, null, null},
-                {EnumAbsorber.PROTON, null, EnumAbsorber.PROTON}
+                {PROTON, null, NEUTRON},
+                {null, null, null}
+        }).register();
+
+        new ColliderRecipe(1, new ItemStack(Items.GUNPOWDER), new EnumAbsorber[][]{
+                {PROTON, null, null},
+                {null, null, null},
+                {null, NEUTRON, null}
+        }).register();
+
+        new ColliderRecipe(1, new ItemStack(Items.STRING), new EnumAbsorber[][]{
+                {null, PROTON, null},
+                {null, null, null},
+                {null, NEUTRON, null}
+        }).register();
+
+        new ColliderRecipe(1, new ItemStack(Blocks.SAPLING, 1, 0), new EnumAbsorber[][]{
+                {PROTON, NEUTRON, null},
+                {null, null, null},
+                {null, null, null}
+        }).register();
+
+        new ColliderRecipe(1, new ItemStack(Blocks.SAPLING, 1, 1), new EnumAbsorber[][]{
+                {PROTON, null, NEUTRON},
+                {null, null, null},
+                {null, null, null}
+        }).register();
+
+        new ColliderRecipe(1, new ItemStack(Blocks.SAPLING, 1, 1), new EnumAbsorber[][]{
+                {PROTON, null, NEUTRON},
+                {null, null, null},
+                {null, null, null}
+        }).register();
+
+        new ColliderRecipe(1, new ItemStack(Blocks.SAPLING, 1, 2), new EnumAbsorber[][]{
+                {PROTON, null, null},
+                {null, null, NEUTRON},
+                {null, null, null}
+        }).register();
+
+        new ColliderRecipe(1, new ItemStack(Blocks.SAPLING, 1, 3), new EnumAbsorber[][]{
+                {PROTON, null, null},
+                {null, null, null},
+                {null, null, NEUTRON}
+        }).register();
+
+        new ColliderRecipe(1, new ItemStack(Blocks.SAPLING, 1, 4), new EnumAbsorber[][]{
+                {PROTON, null, null},
+                {NEUTRON, null, null},
+                {null, null, null}
+        }).register();
+
+        new ColliderRecipe(1, new ItemStack(Blocks.SAPLING, 1, 5), new EnumAbsorber[][]{
+                {PROTON, null, null},
+                {null, null, null},
+                {null, NEUTRON, null}
         }).register();
 
         new ColliderRecipe(1, new ItemStack(Blocks.SAND, 4), new EnumAbsorber[][] {
                 {null, null, null},
-                {null, null, EnumAbsorber.NEUTRON},
-                {null, null, EnumAbsorber.PROTON}
+                {null, null, NEUTRON},
+                {null, null, PROTON}
+        }).register();
+
+        new ColliderRecipe(1, new ItemStack(Blocks.GRAVEL, 4), new EnumAbsorber[][] {
+                {null, null, null},
+                {null, null, PROTON},
+                {null, null, NEUTRON}
+        }).register();
+
+        new ColliderRecipe(1, new ItemStack(Blocks.DIRT, 4), new EnumAbsorber[][] {
+                {null, PROTON, null},
+                {null, null, null},
+                {NEUTRON, null, null}
+        }).register();
+
+        new ColliderRecipe(1, new ItemStack(Items.COAL), new EnumAbsorber[][] {
+                {NEUTRON, null, PROTON},
+                {null, null, null},
+                {null, null, null}
+        }).register();
+
+        new ColliderRecipe(2, new ItemStack(Blocks.NETHER_BRICK, 4), new EnumAbsorber[][] {
+                {PROTON, null, NEUTRON},
+                {PROTON, null, null},
+                {NEUTRON, null, null}
+        }).register();
+
+        new ColliderRecipe(2, new ItemStack(Blocks.SOUL_SAND, 4), new EnumAbsorber[][] {
+                {PROTON, PROTON, null},
+                {null, null, null},
+                {null, NEUTRON, NEUTRON}
+        }).register();
+
+        new ColliderRecipe(2, new ItemStack(Blocks.GRASS), new EnumAbsorber[][] {
+                {NEUTRON, null, PROTON},
+                {NEUTRON, null, null},
+                {PROTON, null, null}
+        }).register();
+
+        new ColliderRecipe(2, new ItemStack(Items.WHEAT_SEEDS), new EnumAbsorber[][]{
+                {PROTON, NEUTRON, null},
+                {PROTON, null, NEUTRON},
+                {null, null, null}
+        }).register();
+
+        new ColliderRecipe(2, new ItemStack(Items.MELON_SEEDS), new EnumAbsorber[][]{
+                {PROTON, null, NEUTRON},
+                {PROTON, null, NEUTRON},
+                {null, null, null}
+        }).register();
+
+        new ColliderRecipe(2, new ItemStack(Items.PUMPKIN_SEEDS), new EnumAbsorber[][]{
+                {PROTON, null, null},
+                {PROTON, null, null},
+                {NEUTRON, null, NEUTRON}
+        }).register();
+
+        new ColliderRecipe(2, new ItemStack(Items.CARROT), new EnumAbsorber[][]{
+                {null, PROTON, PROTON},
+                {null, null, null},
+                {NEUTRON, null, NEUTRON}
+        }).register();
+
+        new ColliderRecipe(2, new ItemStack(Items.POTATO), new EnumAbsorber[][]{
+                {NEUTRON, null, PROTON},
+                {null, null, NEUTRON},
+                {null, PROTON, null}
+        }).register();
+
+        new ColliderRecipe(2, new ItemStack(Items.BEETROOT_SEEDS), new EnumAbsorber[][]{
+                {null, NEUTRON, PROTON},
+                {null, null, null},
+                {NEUTRON, null, PROTON}
+        }).register();
+
+        new ColliderRecipe(2, new ItemStack(Items.DYE), new EnumAbsorber[][]{
+                {PROTON, null, NEUTRON},
+                {null, null, null},
+                {NEUTRON, null, PROTON}
+        }).register();
+
+        new ColliderRecipe(2, new ItemStack(Items.DYE, 1, 3), new EnumAbsorber[][]{
+                {PROTON, null, null},
+                {NEUTRON, null, NEUTRON},
+                {PROTON, null, null}
+        }).register();
+
+        new ColliderRecipe(2, ItemMaterial.instance().getSubItemStack("metal_chunk"), new EnumAbsorber[][]{
+                {NEUTRON, null, null},
+                {PROTON, null, PROTON},
+                {NEUTRON, null, null}
         }).register();
 
         new ColliderRecipe(3, ItemMaterial.instance().getSubItemStack("mystical_gem"), new EnumAbsorber[][]{
-                {EnumAbsorber.NEUTRON, EnumAbsorber.NEUTRON, null},
+                {NEUTRON, NEUTRON, null},
                 {null, null, null},
-                {null, EnumAbsorber.PROTON, EnumAbsorber.PROTON}
+                {null, PROTON, PROTON}
+        }).register();
+
+        new ColliderRecipe(3, new ItemStack(Items.BLAZE_ROD), new EnumAbsorber[][]{
+                {null, PROTON, null},
+                {PROTON, null, NEUTRON},
+                {null, NEUTRON, null}
+        }).register();
+
+        new ColliderRecipe(4, ItemMaterial.instance().getSubItemStack("little_ghast_drop"), new EnumAbsorber[][]{
+                {PROTON, PROTON, NEUTRON},
+                {null, null, null},
+                {NEUTRON,NEUTRON, PROTON}
         }).register();
     }
 }
